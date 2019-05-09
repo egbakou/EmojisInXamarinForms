dd15f0696d824ac831c2de648b1fd69094ee0d92
egbakou <egbakoukodjolaurent@gmail.com>
2019-05-09 07:34:35 +0000

    use of Binding feature added.

---
+++ ViewModels/MainPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using EmojiInXaml.Helpers;
+using EmojiInXaml.Models;
+
+namespace EmojiInXaml.ViewModels
+{
+    public class MainPageViewModel : ObservableObject
+    {
+        public MainPageViewModel()
+        {
+            Emoji = new Emoji(0x1F914).ToString();
+        }
+
+
+        private string _emoji;
+        public string Emoji
+        {
+            get { return _emoji; }
+            set { SetProperty(ref _emoji, value); }
+        }
+    }
+}
