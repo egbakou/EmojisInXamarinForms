ed7801938879a0d4533773d2149fd86c9eac38b0
egbakou <egbakoukodjolaurent@gmail.com>
2019-05-09 07:33:35 +0000

    .gitignore and .gitattributes added.

---
+++ EmojiInXaml/EmojiInXaml/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using EmojiInXaml.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EmojiInXaml
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel();
+        }
+    }
+}
